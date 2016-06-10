7671fb4656fa8a2484dbcdaed3b2db3696fdee52
Leandro Utiyama <leandro@leandrouti.net>
2016-06-10 11:31:57 +0900

    changing the way getting data

--- stock_info_class.php
+++ stock_info_class.php
@@ -5,38 +5,93 @@
 */
 	class Stock{
 
+		private static function getPageData($url, $parameter){
+			$full_url = $url . '?';
+			if(is_array($parameter)){
+				$full_url .= $parameter[0];
+				for($i = 1; $i < sizeof($parameter); $i++){
+					$full_url .= "&{$parameter[$i]}";
+				}
+			}else{
+				$full_url = "$url?$parameter";
+			}
+			return (file_get_contents($full_url)) ? file_get_contents($full_url) : false;
+		}
+
 		public static function get_price($code, $json = false){
 			$res_array = array();
 			if(is_array($code)){
 				foreach($code as $cd){
-					$res_array[] = self::getPriceByCode($cd);
+					$res_array[] = self::getPriceFromCode($cd);
 				}
 				return ($json) ? json_encode($res_array, JSON_UNESCAPED_UNICODE) : $res_array;
 			}else{
-				return ($json) ? json_encode(self::getPriceByCode($code), JSON_UNESCAPED_UNICODE) : self::getPriceByCode($code);
+				return ($json) ? json_encode(self::getPriceFromCode($code), JSON_UNESCAPED_UNICODE) : self::getPriceFromCode($code);
 			}
 		}
 
 		public static function get_code_list($str){
+			//echo self::getPageData("http://info.finance.yahoo.co.jp/search/", array("query=イオン", "p=6"));
 			return self::GetCodeByName($str);
 		}
 
 		private static function getCodeByName($name){
-			$url = "http://info.finance.yahoo.co.jp/search/?query={$name}";
-			$page = file_get_contents($url);
+			$url = "http://info.finance.yahoo.co.jp/search/";
+			$firstPage = self::getPageData($url, "query={$name}");
 			$results = array();
 			$pageResults = array();
-			$num_pages = preg_match_all('/&p=(\d)"/', $page, $pageResults);
+			$allResults = array();
+
+			//get num of page
+			$num_pages = preg_match_all('/&p=(\d)"/', $firstPage, $pageResults);
+			//clean the results of page results
 			$pageResults2 = array();
 			foreach($pageResults[0] as $value){
-				$pageResults2[] = str_replace(array("&p=", "\""), "", $value);
+				$value = str_replace(array("&p=", "\""), "", $value);
+				if(!in_array($value, $pageResults2)){
+					$pageResults2[] = $value;
+				}
 			}
 
-			//echo preg_match_all('/name highlight">(.*)</', $page, $results);
-			//var_dump($pageResults2);
-			//needs to get all the results for each page
+			//=================Get Name, Code, Market from all pages======================//
+
+			//preg_match_all('<dt class="price yjXXL">(.*?)</dt>', $)
+
+			echo $firstPage;
+			$temp = array();
+			//get data from page 1
+			preg_match_all('#name highlight">(.*)</span> |<dt class="price yjXXL">(.*)</dt>#', $firstPage, $results);
+			$temp = $results[1];
+			//preg_match_all('#<dt class="price yjXXL">(.*)</dt>#', $firstPage, $results);
+			//var_dump($results);
+			var_dump($temp);
+			$allResults = $results[1];
+/*
+			//get data from the rest of pages
+			foreach($pageResults2 as $pageNum){
+				$pageData = self::getPageData($url, array("query={$name}", "p={$pageNum}"));
+				preg_match_all('/name highlight">(.*)/', $pageData, $results);
+				$allResults = array_merge($results[1], $allResults);
+			}
+*/
+/*
+			//cleaning data
+			$output = array();
+			foreach($allResults as $res){
+				$match = array();
+				$match_price = array();
+				preg_match_all('#^(.*)</span> <span class="secondInfo"><em class="code highlight">\[(.*)\]</em> <em class="market yjSt">- (.*)</em></span></a>#', $res, $match);
+				$ar = array('market' => $match[3][0],'c_name' => $match[1][0], 'code' => $match[2][0]);
+				$output[] = $ar;
+			}
+
+			var_dump($output);
+			//return $allResults;
+*/
 		}
-		private static function getPriceByCode($code){
+
+
+		private static function getPriceFromCode($code){
 			$result = array();
 			$url = "http://stocks.finance.yahoo.co.jp/stocks/detail/?code=" . $code . '.T';
 			$page = file_get_contents($url);
